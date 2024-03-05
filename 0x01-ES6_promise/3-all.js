23d9a307dbefb4cbd23d671cc9da5af9240434c3
Abanoubo1 <abanoubwageh88@gmail.com>
2024-03-05 21:27:33 +0000

    all 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,20 +1,12 @@
+/* eslint-disable no-console */
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup () {
-  let body;
-  let firstName;
-  let lastName;
+export default function handleProfileSignup() {
+  const photo = uploadPhoto();
+  const user = createUser();
 
-  return uploadPhoto()
-    .then((data) => {
-      body = data.body;
-      createUser()
-        .then((data) => {
-          firstName = data.firstName;
-          lastName = data.lastName;
-          console.log(`${body} ${firstName} ${lastName}`);
-        })
-        .catch(() => console.log('Signup system offline'));
-    })
-    .catch(() => console.log('Signup system offline'));
+  return Promise.all([photo, user]).then((res) => {
+    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+  })
+    .catch(() => { console.log('Signup system offline'); });
 }
