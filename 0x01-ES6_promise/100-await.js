aff1aa62b501a9cf3f649fa354339f743082a6a4
Abanoubo1 <abanoubwageh88@gmail.com>
2024-03-05 22:35:32 +0000

    100 awai

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,17 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function asyncUploadUser () {
-    try {
-    const photo = await uploadPhoto();
-    const user = await createUser();
+export default async function asyncUploadUser() {
+  try {
+    const prom1 = await uploadPhoto();
+    const prom2 = await createUser();
 
-    return Promise.resolve({
-        photo, user
-    });
-    } catch (err) {
-    return Promise.resolve({
-        photo: null,
-        user: null
-    });
-    }
+    return ({ photo: prom1, user: prom2 });
+  } catch (error) {
+    return ({ photo: null, user: null });
+  }
 }
